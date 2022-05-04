b90e66b3e0b000eea2a3b2fe1aa0d5ee9fb1a6a2
jeonbyeongmin <qudals7613@gmail.com>
2022-05-04 13:43:03 +0900

    chore : remove tailwind & update config file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import type {NextPage} from 'next';
-
-import tw from 'twin.macro';
+import styled from '@emotion/styled';
 
 const Home: NextPage = () => {
   return (
@@ -10,7 +9,9 @@ const Home: NextPage = () => {
   );
 };
 
-const Wrapper = tw.section`flex w-full`;
-const Column = tw.div`w-1/2`;
+const Wrapper = styled.section`
+  padding: 20px;
+`;
+const Column = styled.div``;
 
 export default Home;
