964e9ffb31391e7f173fdf2e7f92ca31a83f6627
sumi.byun <selina2000@naver.com>
2022-05-17 02:10:17 +0900

    chore : chakra ui 설치

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,17 +2,18 @@ import {Global} from '@emotion/react';
 import type {AppProps} from 'next/app';
 import Head from 'next/head';
 import globalStyles from 'styles/global-styles';
+import { ChakraProvider } from '@chakra-ui/react'
 
 function App({Component, pageProps}: AppProps) {
   return (
-    <>
+    <ChakraProvider>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>boilerplate</title>
       </Head>
       <Global styles={globalStyles} />
       <Component {...pageProps} />
-    </>
+    </ChakraProvider>
   );
 }
 
