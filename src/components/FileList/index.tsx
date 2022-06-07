f57fe25e91c259029281ad22f8f87f5f0777f40e
sumi.byun <selina2000@naver.com>
2022-06-07 22:20:37 +0900

    feat : 요리정보, 재료 UI

---
+++ src/components/FileList/index.tsx
@@ -0,0 +1,42 @@
+import {Box, Heading, Flex, Input} from '@chakra-ui/react';
+import styled from '@emotion/styled';
+import {PlusOutLineIcon} from 'components/Icon';
+import Ingredient from 'components/Ingredient';
+
+export function IngredientList() {
+  return (
+    <Box marginY="10">
+      <Heading marginY="5">재료 </Heading>
+      <Flex gap={4}>
+        <Ingredient
+          name="사과"
+          amount="1개"
+          img="https://img.lovepik.com/element/40162/3354.png_860.png"
+        ></Ingredient>
+        <Ingredient
+          name="사과"
+          amount="1개"
+          img="https://img.lovepik.com/element/40162/3354.png_860.png"
+        ></Ingredient>
+        <Ingredient
+          name="사과"
+          amount="1개"
+          img="https://img.lovepik.com/element/40162/3354.png_860.png"
+        ></Ingredient>{' '}
+        <Input type={'file'} id={`ingredientFileInput`} hidden />
+        <IngredientInputLabel htmlFor={`ingredientFileInput`}>
+          <PlusOutLineIcon />
+        </IngredientInputLabel>
+      </Flex>
+    </Box>
+  );
+}
+const IngredientInputLabel = styled(Box)`
+  width: 6rem;
+  height: 6rem;
+  border: 1px solid #b9b9b9;
+  border-radius: 50%;
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+`;
