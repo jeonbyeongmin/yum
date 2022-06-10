a3d21725684f8ab272473e78a406b5a394a11ac6
woo-jk <201702042@o.cnu.ac.kr>
2022-06-10 21:40:59 +0900

    feat: recipeList UI 컴포넌트 추가

--- src/components/Icon/index.tsx
+++ src/components/Icon/index.tsx
@@ -1,11 +1,23 @@
-import React from 'react';
-import {IoCartOutline, IoPersonOutline, IoSearchOutline} from 'react-icons/io5';
+import React from "react";
+import {
+  IoCartOutline,
+  IoPersonOutline,
+  IoSearchOutline,
+  IoBookmarkOutline,
+  IoHeartOutline,
+} from "react-icons/io5";
 import {
   AiFillPlusCircle,
   AiOutlineClose,
   AiOutlineMinus,
   AiOutlinePlus,
-} from 'react-icons/ai';
+} from "react-icons/ai";
+import {
+  MdOutlineArrowForwardIos,
+  MdOutlineArrowBackIos,
+} from "react-icons/md";
+import styled from "@emotion/styled";
+
 interface Iicon {
   size?: number;
   color?: string;
@@ -21,16 +33,48 @@ export function UserIcon() {
 export function PlusIcon() {
   return <AiFillPlusCircle size={50} />;
 }
-export function PlusOutLineIcon({size = 30, color = '#000'}: Iicon) {
+export function PlusOutLineIcon({ size = 30, color = "#000" }: Iicon) {
   return <AiOutlinePlus size={size} color={color} />;
 }
-export function MinusIcon({size = 30, color = '#000'}: Iicon) {
+export function MinusIcon({ size = 30, color = "#000" }: Iicon) {
   return <AiOutlineMinus size={size} color={color} />;
 }
-export function CloseIcon({size = 20, color = '#000'}: Iicon) {
+export function CloseIcon({ size = 20, color = "#000" }: Iicon) {
   return <AiOutlineClose size={size} color={color} />;
 }
 
-export function SearchIcon({size}: Iicon) {
+export function SearchIcon({ size }: Iicon) {
   return <IoSearchOutline size={size ?? 20} />;
 }
+
+export function LikeIcon({ size = 25, color = "gray" }: Iicon) {
+  return <IoHeartOutline size={size} color={color} />;
+}
+
+export function BookmarkIcon({ size = 25, color = "gray" }: Iicon) {
+  return <IoBookmarkOutline size={size} color={color} />;
+}
+
+export function RightArrowIcon({ size = 25 }: Iicon) {
+  return (
+    <ArrowCircle>
+      <MdOutlineArrowForwardIos size={size} />
+    </ArrowCircle>
+  );
+}
+
+export function LeftArrowIcon({ size = 25 }: Iicon) {
+  return (
+    <ArrowCircle>
+      <MdOutlineArrowBackIos size={size} />
+    </ArrowCircle>
+  );
+}
+
+const ArrowCircle = styled.div`
+  padding: 7px;
+  border-radius: 50%;
+  border: 0.5px solid lightgray;
+  box-shadow: 0 3px 5px gray;
+  background-color: white;
+`;
