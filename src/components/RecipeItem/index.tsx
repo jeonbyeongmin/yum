4ecdf312e1f86a5c0821088e039a6bd6619883a0
woo-jk <201702042@o.cnu.ac.kr>
2022-06-11 05:38:03 +0900

    feat: like, bookmark 클릭이벤트 / item 이미지 hover시 효과 추가

--- src/components/RecipeItem/index.tsx
+++ src/components/RecipeItem/index.tsx
@@ -1,35 +1,62 @@
 import { Box, Image, Text, VStack, Flex, Avatar } from "@chakra-ui/react";
 import { BookmarkIcon, LikeIcon } from "components/Icon";
 import { IRecipeItem } from "components/RecipeItemList";
+import { scaleCorrectors } from "framer-motion/types/projection/styles/scale-correction";
+import { useState } from "react";
 
 interface IRecipeData {
   data: IRecipeItem;
 }
 function RecipeItem({ data }: IRecipeData) {
+  const [liked, setLiked] = useState<boolean>(data.liked);
+  const [likeCount, setLikeCount] = useState<number>(data.likeCount);
+  const [bookmarked, setBookmarked] = useState<boolean>(data.bookmarked);
+
+  function likeClick() {
+    setLiked(!liked);
+    liked ? setLikeCount(likeCount - 1) : setLikeCount(likeCount + 1);
+  }
+  function bookmarkClick() {
+    setBookmarked(!bookmarked);
+  }
   return (
     <VStack width={"240px"}>
-      <Image
-        src={data.img}
-        width={"240px"}
-        height={"240px"}
-        borderRadius={5}
-        objectFit={"cover"}
-      />
+      <Box overflow="hidden" cursor="pointer">
+        <Image
+          src={data.img}
+          width={"240px"}
+          height={"240px"}
+          borderRadius={5}
+          objectFit={"cover"}
+          transition="all 0.2s"
+          _hover={{
+            transform: "scale(1.1)",
+          }}
+        />
+      </Box>
       <Box p={3} width={"240px"}>
         <Flex justifyContent="space-between">
           <Flex alignItems="center">
-            <Box cursor={"pointer"}>
-              <LikeIcon activate={data.liked} />
+            <Box cursor={"pointer"} onClick={likeClick}>
+              <LikeIcon activate={liked} />
             </Box>
             <Text fontSize="xl" color="gray.500" ml={2}>
-              {data.likeCount}
+              {likeCount}
             </Text>
           </Flex>
-          <Box cursor={"pointer"}>
-            <BookmarkIcon activate={data.bookmarked} />
+          <Box cursor={"pointer"} onClick={bookmarkClick}>
+            <BookmarkIcon activate={bookmarked} />
           </Box>
         </Flex>
-        <Text mt="5" fontWeight="semibold" fontSize="2xl" lineHeight="tight">
+        <Text
+          mt="5"
+          fontWeight="semibold"
+          fontSize="2xl"
+          lineHeight="tight"
+          transition="all 0.2s"
+          cursor="pointer"
+          _hover={{ opacity: "0.6" }}
+        >
           {data.name}
         </Text>
         <Text mt="5" color="gray.500" fontSize="xl" minH="25px">
