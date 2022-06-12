68de06725a23eaaabfe6edbf22847e36d11f1295
jeonbyeongmin <qudals7613@gmail.com>
2022-06-12 20:37:27 +0900

    feat: add navigation

--- src/components/IconBtn/index.tsx
+++ src/components/IconBtn/index.tsx
@@ -1,7 +1,5 @@
 import {IconButton} from '@chakra-ui/react';
-import {css} from '@emotion/react';
-import styled from '@emotion/styled';
-import React, {ReactNode} from 'react';
+import React from 'react';
 
 interface IiconBtn {
   icon: JSX.Element;
@@ -12,8 +10,11 @@ interface IiconBtn {
   size?: string;
 }
 
-const IconBtn = ({icon, label, type,borderColor, bgColor, size}: IiconBtn) => {
-  const DisplayProps = type === 'mobile' ? {base:'inline-flex', md:'none'} : {base:'inline-flex'}
+const IconBtn = ({icon, label, type, borderColor, bgColor, size}: IiconBtn) => {
+  const DisplayProps =
+    type === 'mobile'
+      ? {base: 'inline-flex', md: 'none'}
+      : {base: 'inline-flex'};
   return (
     <IconButton
       borderRadius={'50%'}
@@ -25,7 +26,7 @@ const IconBtn = ({icon, label, type,borderColor, bgColor, size}: IiconBtn) => {
         borderColor: borderColor ?? '#efefef',
       }}
       _focus={{
-        boxShadow: 'none'
+        boxShadow: 'none',
       }}
       width={size ?? '35px'}
       height={size ?? '35px'}
