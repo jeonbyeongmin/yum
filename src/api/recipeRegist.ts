ae4c834568daada229907fb729bc870b2ce62719
sumi.byun <selina2000@naver.com>
2022-06-13 00:04:30 +0900

    feat : 이미지 업로드 성공
    그렇지만 왜 된건지 알수없다;;

--- src/api/recipeRegist.ts
+++ src/api/recipeRegist.ts
@@ -1,7 +1,7 @@
 // import {getDatabase, ref, set} from 'firebase/database';
 // const db = getDatabase().collection('recipe');
 import {collection, addDoc} from 'firebase/firestore';
-import {ref, uploadBytes} from 'firebase/storage';
+import {getDownloadURL, ref, uploadBytes, uploadString} from 'firebase/storage';
 import {db, storage} from '../firebase';
 // const storage = firebase.storage();
 
@@ -9,21 +9,19 @@ export async function addRecipe(data: any) {
   try {
     console.log(storage);
     console.log('data', data);
-    // const storageRef = storage.ref();
     const storageRef = ref(storage, 'image/' + `${data.name}`);
-    // const savepath = storageRef.child('image/' + `${data.name}`);
-    // const imagesRef = ref(storageRef, 'image/' + `${data.name}`));
-    // const upload = imagesRef.put(data.imgs[0]);
-    // console.log(upload);
     const metadata = {
       contentType: 'image/jpeg',
     };
-    // Upload the file and metadata
-    console.log(data.cookingImgs[0]);
     const uploadTask = uploadBytes(storageRef, data.cookingImgs[0], metadata);
     console.log(uploadTask);
     // const docRef = await addDoc(collection(db, 'recipe'), data);
     // console.log('Document written with ID: ', docRef.id);
+
+    const res = await uploadString(storageRef, data.cookingImgs[0], 'data_url');
+    const fileURL = await getDownloadURL(res.ref);
+
+    console.log(fileURL  );
   } catch (e) {
     console.error('Error adding document: ', e);
   }
