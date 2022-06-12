0d96b17bbb26bb4f9d3b4fa23c59dceb766b498c
sumi.byun <selina2000@naver.com>
2022-06-13 06:47:24 +0900

    fix : recipe ts

--- src/types/recipe.ts
+++ src/types/recipe.ts
@@ -12,18 +12,10 @@ export interface IRecipeStep {
   img: string;
 }
 
-// export interface IRecipeInfo extends ICookingInfo {
-//   // imgs: [];
-//   infredients: [1, 2];
-//   desc: '요즘 백파더 백종원 에그치즈토스트가 대유행이라죠\n칼질도 필요 없고, 원팬으로 만들기로 간단하지만\n맛은 대박~ 요건 꼭 만들어 보세요\n에그드랍 가실 필요 없어요^^';
-//   steps: IRecipeStep[];
-//   cookingImgs: string[];
-// }
-
 export interface IRecipeInfo {
   cookingInfo: ICookingInfo;
-  steps: IRecipeStep;
-  ingredients: any;
+  steps: IRecipeStep[];
+  ingredients: any[];
   user: {
     uid: string;
     name: string;
