85e447269fa9329241157fa9848370f57904a786
jeonbyeongmin <qudals7613@gmail.com>
2022-06-13 11:33:40 +0900

    feat : login 에 reack-hook-form/yup 적용

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -4,6 +4,8 @@ import {
   Center,
   Divider,
   Flex,
+  FormControl,
+  FormErrorMessage,
   HStack,
   Input,
   Text,
@@ -11,8 +13,28 @@ import {
 } from '@chakra-ui/react';
 import Link from 'next/link';
 import React from 'react';
+import {useForm} from 'react-hook-form';
+import * as yup from 'yup';
+import {yupResolver} from '@hookform/resolvers/yup';
 
 function Login() {
+  const schema = yup.object().shape({
+    email: yup.string().email().required(),
+    password: yup.string().min(7).max(10).required(),
+  });
+
+  const {
+    handleSubmit,
+    register,
+    formState: {errors, isSubmitting},
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  function onSubmit() {
+    console.log('submit');
+  }
+
   return (
     <Flex
       justifyContent="center"
@@ -41,50 +63,66 @@ function Login() {
         <Text paddingY="3rem" fontSize="4xl" fontWeight="extrabold">
           로그인
         </Text>
-        <VStack spacing="5">
-          <Input
-            placeholder="이메일"
-            _placeholder={{
-              color: 'gray.300',
-              fontSize: '2xl',
-              fontWeight: 'medium',
-            }}
-            shadow="sm"
-            paddingLeft="10"
-            paddingY="12"
-            size="lg"
-            borderColor="gray.200"
-            borderRadius="xl"
-          />
-          <Input
-            placeholder="비밀번호"
-            _placeholder={{
-              color: 'gray.300',
-              fontSize: '2xl',
-              fontWeight: 'medium',
-            }}
-            shadow="sm"
-            paddingLeft="10"
-            paddingY="12"
-            size="lg"
-            borderColor="gray.200"
-            borderRadius="xl"
-          />
-        </VStack>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <VStack spacing="5">
+            <FormControl isInvalid={errors.email}>
+              <Input
+                placeholder="이메일"
+                _placeholder={{
+                  color: 'gray.300',
+                  fontSize: '2xl',
+                  fontWeight: 'medium',
+                }}
+                shadow="sm"
+                paddingLeft="10"
+                paddingY="12"
+                size="lg"
+                borderColor="gray.200"
+                borderRadius="xl"
+                {...register('email')}
+              />
+              <FormErrorMessage>
+                {errors.email && errors.email.message}
+              </FormErrorMessage>
+            </FormControl>
+            <FormControl isInvalid={errors.password}>
+              <Input
+                placeholder="비밀번호"
+                _placeholder={{
+                  color: 'gray.300',
+                  fontSize: '2xl',
+                  fontWeight: 'medium',
+                }}
+                shadow="sm"
+                paddingLeft="10"
+                paddingY="12"
+                size="lg"
+                borderColor="gray.200"
+                borderRadius="xl"
+                {...register('password')}
+              />
+              <FormErrorMessage>
+                {errors.password && errors.password.message}
+              </FormErrorMessage>
+            </FormControl>
+          </VStack>
 
-        <Button
-          width="full"
-          marginY="10"
-          borderRadius="xl"
-          paddingY="12"
-          fontSize="xl"
-          colorScheme="orange"
-          _focus={{border: 'none'}}
-        >
-          <Text fontSize="2xl" fontWeight="bold">
-            로그인
-          </Text>
-        </Button>
+          <Button
+            type="submit"
+            isLoading={isSubmitting}
+            width="full"
+            marginY="10"
+            borderRadius="xl"
+            paddingY="12"
+            fontSize="xl"
+            colorScheme="orange"
+            _focus={{border: 'none'}}
+          >
+            <Text fontSize="2xl" fontWeight="bold">
+              로그인
+            </Text>
+          </Button>
+        </form>
 
         <HStack alignContent="cetner" justifyContent="center">
           <a>
