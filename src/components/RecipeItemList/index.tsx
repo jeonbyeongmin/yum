45d3ad0da5e5b3fc0e6ac1bba85264638d6e7e47
Byeongmin Jeon <qudals7613@gmail.com>
2022-06-20 01:09:06 +0900

    landing page 캐로셀 적용 (#58)

--- src/components/RecipeItemList/index.tsx
+++ src/components/RecipeItemList/index.tsx
@@ -1,7 +1,8 @@
-import {Box, Flex, Heading, HStack} from '@chakra-ui/react';
+import {Box, Flex, Heading, HStack, IconButton} from '@chakra-ui/react';
 import {LeftArrowIcon, RightArrowIcon} from 'components/Icon';
 import RecipeItem from 'components/RecipeItem';
 import Link from 'next/link';
+import {useEffect, useState} from 'react';
 import {RecipeData} from 'types/recipe';
 
 interface IRecipeItemList {
@@ -10,27 +11,78 @@ interface IRecipeItemList {
 }
 
 function RecipeItemList({title, recipes}: IRecipeItemList) {
+  const [margin, setMargin] = useState(0);
+  const [current, setCurrent] = useState(1);
+  const [leftHidden, setLeftHidden] = useState(true);
+  const [rightHidden, setRightHidden] = useState(false);
+  const [isLast, setIsLast] = useState(false);
+
+  const handleLeftArrow = () => {
+    setMargin(prev => prev + 1039);
+    setCurrent(prev => prev - 1);
+  };
+
+  const handleRightArrow = () => {
+    setMargin(prev => prev - 1039);
+    setCurrent(prev => prev + 1);
+  };
+
+  useEffect(() => {
+    margin < 0 ? setLeftHidden(false) : setLeftHidden(true);
+  }, [margin]);
+
+  useEffect(() => {
+    current > recipes.length / 4 ? setIsLast(true) : setIsLast(false);
+  }, [current, recipes]);
+
+  useEffect(() => {
+    isLast ? setRightHidden(true) : setRightHidden(false);
+  }, [isLast]);
+
   return (
     <Box pos="relative" mb={20}>
       <Heading size="xl" marginY="10" fontWeight="black">
         {title}
       </Heading>
-      <HStack gap={5}>
-        {recipes?.map(data => (
-          <Link href={`/recipe/${data.docId}`} key={data.docId}>
-            <a>
-              <RecipeItem key={data.docId} data={data} />
-            </a>
-          </Link>
-        ))}
-      </HStack>
+      <Box overflow="hidden">
+        <HStack
+          gap={'15px'}
+          maxWidth="1024px"
+          marginLeft={margin}
+          transition="ease 0.5s"
+        >
+          {recipes?.map(data => (
+            <Link href={`/recipe/${data.docId}`} key={data.docId}>
+              <a>
+                <RecipeItem key={data.docId} data={data} />
+              </a>
+            </Link>
+          ))}
+        </HStack>
+      </Box>
       <Flex>
-        <Box pos="absolute" top="35%" left={-8}>
-          <LeftArrowIcon />
-        </Box>
-        <Box pos="absolute" top="35%" right={-8}>
-          <RightArrowIcon />
-        </Box>
+        <IconButton
+          aria-label="left arrow"
+          hidden={leftHidden}
+          icon={<LeftArrowIcon />}
+          pos="absolute"
+          top="35%"
+          left={-8}
+          _focus={{border: 'none'}}
+          onClick={handleLeftArrow}
+          variant="unstyled"
+        />
+        <IconButton
+          aria-label="right arrow"
+          hidden={rightHidden}
+          icon={<RightArrowIcon />}
+          pos="absolute"
+          top="35%"
+          right={-8}
+          _focus={{border: 'none'}}
+          onClick={handleRightArrow}
+          variant="unstyled"
+        />
       </Flex>
     </Box>
   );
